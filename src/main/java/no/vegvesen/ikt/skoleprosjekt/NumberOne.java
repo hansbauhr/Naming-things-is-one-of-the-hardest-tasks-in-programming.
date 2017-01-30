6397bf5b503ae05226966d06fd8d08a2c16d1204
farlusiva <hhhkko5@gmail.com>
2017-01-30 13:27:50 +0100

    Added a thing

---
+++ src/main/java/no/vegvesen/ikt/skoleprosjekt/NumberOne.java
@@ -0,0 +1,15 @@
+package no.vegvesen.ikt.skoleprosjekt;
+
+public class NumberOne {
+	
+	public static void heiVerdwen() {
+		System.out.println("Hallo Verdwen!");
+	}
+	
+	public static void main(String[] args){
+		
+		heiVerdwen();
+		
+	}
+
+}
