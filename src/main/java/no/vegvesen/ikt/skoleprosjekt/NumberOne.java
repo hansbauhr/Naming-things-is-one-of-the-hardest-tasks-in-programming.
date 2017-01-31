d7207afa104ce4d6d8033eb9e1ba55d51045875e
farlusiva <hhhkko5@gmail.com>
2017-01-31 09:02:26 +0100

    Fixed spelling mistakes

--- src/main/java/no/vegvesen/ikt/skoleprosjekt/NumberOne.java
+++ src/main/java/no/vegvesen/ikt/skoleprosjekt/NumberOne.java
@@ -2,13 +2,13 @@ package no.vegvesen.ikt.skoleprosjekt;
 
 public class NumberOne {
 	
-	public static void heiVerdwen() {
-		System.out.println("Hallo Verdwen!");
+	public static void HalloVerden() {
+		System.out.println("Hallo verden!");
 	}
 	
 	public static void main(String[] args){
 		
-		heiVerdwen();
+		HalloVerden();
 		
 	}
 
